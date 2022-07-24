4a5e2e9d2b2f26a228d2c85e2c9af9bfea021778
doniaskima <doniaskima344@gmail.com>
2022-07-24 22:22:26 +0100

    some progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,37 @@ import styled from "styled-components";
 import Section from "./Section"
 function Home() {
    return (
-       <Container>
-       <Section/>
-       </Container>
-   )
+     <Container>
+       <Section
+         title="Model S"
+         description="Order online for Touchless elivery "
+         backgroundImg="model-s.jpg"
+         leftBtnText="Custom order"
+         rightBtnText="Existing inventory"
+       />
+       <Section
+         title="Model Y"
+         description="Order online for Touchless elivery "
+         backgroundImg="model-y.jpg"
+         leftBtnText="Custom order"
+         rightBtnText="Existing inventory"
+       />
+       <Section
+         title="Model 3"
+         description="Order online for Touchless elivery "
+         backgroundImg="model-3.jpg"
+         leftBtnText="Custom order"
+         rightBtnText="Existing inventory"
+       />
+       <Section
+         title="Model X"
+         description="Order online for Touchless elivery "
+         backgroundImg="model-x.jpg"
+         leftBtnText="Custom order"
+         rightBtnText="Existing inventory"
+       />
+     </Container>
+   );
 }
  
 
@@ -15,6 +42,4 @@ export default Home
 
 const Container = styled.div`
 height:100vh;
-
-
 `
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,63 +1,84 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) {
   return (
-    <Wrap> 
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model s</h1>
-        <p>Order online for Touchless elivery </p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>
-custom Order
-        </LeftButton>
-        <RightButton>
-Existing Inventory
-        </RightButton>
-      </ButtonGroup>
-      </Wrap>
-  )
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton> {leftBtnText}</LeftButton>
+          <RightButton> {rightBtnText}</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-width:100vw;
-height:100vh;
-background-size:cover;
-background-position:center;
-background-repeat:no-repeat; 
-background-image:url("/images/model-s.jpg");
-display:flex;
-flex-direction:column;
-justify-content:space-between;
-align-_items:center;
-`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
 
 const ItemText = styled.div`
-padding-top:18vh;
-text-align:center;
-
-`
+  padding-top: 18vh;
+  text-align: center;
+`;
 const ButtonGroup = styled.div`
-display:flex;
-margin-bottom:30px;
-
-`
+  display: flex;
+  margin-bottom: 30px;
+  @media (min-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 const LeftButton = styled.div`
-background-color:rgba(23,26,32,0.8);
-height:40px;
-width:256px;
-color:white;
-display:flex;
-justify-content:center;
-align-items:center;
-border-radius:100px;
-opacity:0.85;
-text-transform:uppercase;
-font-size:12px;
-cucursor:pointer;
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
 `;
-const RightButton = styled(LeftButton)``;
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Buttons = styled.div``;
